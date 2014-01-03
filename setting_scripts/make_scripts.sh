5a69a42a2ab913c92431e2983b7263942574d65f
U-waltripracing\donour.sizemore <donour.sizemore@donour-dt.waltripracing.local>
2014-01-03 10:26:14 -0500

    added setting script generator

---
+++ setting_scripts/make_scripts.sh
@@ -0,0 +1,17 @@
+#!/bin/env bash
+
+export NSETS=4
+
+for setnum in `seq 1 $NSETS`;
+do
+    for corner in "LF" "LR" "RF" "RR";
+    do
+	export filename="SET$setnum-$corner.sh" 
+	echo "#!/usr/bin/env bash"        > $filename
+	echo "export SETNUM=$setnum"     >> $filename
+	echo "export POSITION=$corner"   >> $filename
+	cat base.sh                      >> $filename
+    done
+	
+
+done
